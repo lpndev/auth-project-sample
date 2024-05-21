f38b9d44b9092be1a61b0c83409c3277b742be44
Leonardo Diniz <137980570+lpndev@users.noreply.github.com>
2024-05-21 14:21:25 -0700

    feat: creating dynamic home page and get sessions

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,34 @@
-export default function Home() {
-  return <div>Home</div>
+import Link from 'next/link'
+
+import { auth } from '@/auth'
+import { LogInIcon } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { SignIn } from '@/components/auth/sign-in'
+import { ModeToggle } from '@/components/mode-toggle'
+
+export default async function Home() {
+  const session = await auth()
+
+  return (
+    <>
+      <main className='flex h-screen w-full flex-col items-center justify-center'>
+        <div>
+          {!session ? (
+            <SignIn />
+          ) : (
+            <Button asChild variant='default' className='gap-2'>
+              <Link href='/dashboard'>
+                Go to Dashboard
+                <LogInIcon size={16} />
+              </Link>
+            </Button>
+          )}
+        </div>
+      </main>
+      <div className='absolute right-5 top-5'>
+        <ModeToggle />
+      </div>
+    </>
+  )
 }
